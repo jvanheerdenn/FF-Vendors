6858102f3a551100e75ad997004bf156dc92c118
jvanheerdenn <jvanheerden@vectoritcgroup.com>
2021-10-18 13:25:09 +0200

    2 files modified, 1 added, 1 deleted

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { executeOptimizely } from './Files/optimizely.js';
 import { executeVWO } from './Files/VWO.js';
 import { executeTarget } from './Files/adobeTarget.js';
 import { executeFlagship } from './Files/flagship.js';
-import { executeGoogle } from './Files/googleOptimize.js';
+import { executeLaunch } from './Files/launchdarkly.js';
 
 document.getElementById('optimizely').addEventListener('click', () => {
   executeOptimizely();
@@ -23,7 +23,7 @@ document.getElementById('flagship').addEventListener('click', () => {
   executeFlagship();
 });
 
-document.getElementById('google').addEventListener('click', () => {
+document.getElementById('launchdarkly').addEventListener('click', () => {
   console.clear();
-  executeGoogle();
+  executeLaunch();
 });
